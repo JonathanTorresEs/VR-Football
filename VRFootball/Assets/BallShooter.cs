0e4a4f40689a639d190cd0e1321e4b8ffe6deeaf
JonathanTorresEs <jonathan.torres.es@gmail.com>
2019-06-21 11:51:33 -0600

    Added ability to catch ball and arrows to help the player identify the ball while in mid-air

--- VRFootball/Assets/BallShooter.cs
+++ VRFootball/Assets/BallShooter.cs
@@ -2,17 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallShooter : MonoBehaviour {
 
+    // Launch variables
     public float initialAngle = 0.0f;
-    public GameObject target2;
+    public GameObject target;
     private Rigidbody rigid;
+
+    public Image[] arrows;
+    private GameObject player;
+
+    // Hand variables
+    private GameObject hand;
+    private bool heldInHand = false;
+
+
 
     // Use this for initialization
     void Start () {
 
         rigid = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
 
         LookAtTarget();
         LaunchBall();
@@ -21,7 +33,7 @@ public class BallShooter : MonoBehaviour {
     // Make ball face to target
     private void LookAtTarget()
     {
-        Vector3 direction = target2.transform.position - transform.position;
+        Vector3 direction = target.transform.position - transform.position;
 
         Quaternion targetRotation = Quaternion.LookRotation(direction);
         Quaternion lookAt = Quaternion.RotateTowards(transform.rotation, targetRotation, 360f);
@@ -30,8 +42,7 @@ public class BallShooter : MonoBehaviour {
 
     private void LaunchBall()
     {
-
-        Vector3 p = target2.transform.position;
+        Vector3 p = target.transform.position;
 
         float gravity = Physics.gravity.magnitude;
         // Selected angle in radians
@@ -57,6 +68,42 @@ public class BallShooter : MonoBehaviour {
         // Fire!
         rigid.velocity = finalVelocity;
         rigid.AddTorque(transform.forward * 20, ForceMode.VelocityChange);
+    }
+
+
+    // When player catches ball, stop its velocity and torque,
+    // disable its gravity and attach to hand
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "hand_right")
+        {
+            rigid.useGravity = false;
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+
+            gameObject.transform.SetParent(other.transform);
+
+            heldInHand = true;
+            hand = other.gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        if(heldInHand)
+        {
+            transform.position = hand.transform.position;
+            transform.rotation = hand.transform.rotation;
+        } else
+        {
+            Vector3 lookVector = player.transform.position - transform.position;
+            lookVector.x = lookVector.z = 0.0f;
+            foreach(Image image in arrows)
+            {
+                image.transform.LookAt(player.transform.position - lookVector);
+                image.transform.Rotate(0, 180, 0);
+            }
+        }
     }
 
 }
