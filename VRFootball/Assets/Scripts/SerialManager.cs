d2261a72e94ec16461aced07daae54891eb4270f
JonathanTorresEs <jonathan.torres.es@gmail.com>
2019-07-03 09:49:51 -0600

    Removed BT library

--- VRFootball/Assets/Scripts/SerialManager.cs
+++ VRFootball/Assets/Scripts/SerialManager.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
+using UnityEngine.UI;
 
 public class SerialManager : MonoBehaviour {
 
     [SerializeField]
     private string comPort = "COM3";
+    public bool oculusQuestBuild = false;
     public bool playWithSerial = false;
     public SerialPort serial;
 
+    public Text debugText;
+
     // Use this for initialization
     void Start () {
-        if (playWithSerial)
+
+        if (oculusQuestBuild)
+        {
+            try
+            {
+              
+            } catch (System.Exception e)
+            {
+                debugText.text = e.ToString();
+            }
+        } else if (playWithSerial)
         {
             serial = new SerialPort(comPort, 9600);
 
@@ -31,10 +45,14 @@ public class SerialManager : MonoBehaviour {
 
     public void ActivateVRVest()
     {
-        if (playWithSerial && serial.IsOpen)
+       if (playWithSerial && serial.IsOpen)
         {
             serial.Write("4");
         }
     }
 
+    private void Update()
+    {
+
+    }
 }
