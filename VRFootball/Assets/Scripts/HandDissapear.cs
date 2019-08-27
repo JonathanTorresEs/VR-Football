c3d17715146cfeb03b3854a013eff98aa3f3c150
Alejandro Perez G <49079282+AlexPrz28@users.noreply.github.com>
2019-08-27 12:39:06 -0600

    Hide Right Hand on Start Definitive
    this one works

--- VRFootball/Assets/Scripts/HandDissapear.cs
+++ VRFootball/Assets/Scripts/HandDissapear.cs
@@ -7,19 +7,19 @@ public class HandDissapear : MonoBehaviour {
 	public Material mat;
 	private GameObject handRight;
 	// Use this for initialization
-	void Start () {
-		handRight = GameObject.Find ("hand_right_renderPart_0");
+	void Awake () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(handRight != null)
-		{
-			Debug.Log("cambio de material");
-			handRight.GetComponent<Renderer>().material = mat;
-			Destroy(GetComponent<HandDissapear>());
-		}
+			handRight = GameObject.Find("hand_right_renderPart_0");
+
+			if(handRight)
+			{
+				handRight.GetComponent<SkinnedMeshRenderer>().enabled = false;
+				Destroy(GetComponent<HandDissapear>());
+			}
 		
 	}
 }
